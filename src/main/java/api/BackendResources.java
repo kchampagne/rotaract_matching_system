41820e2188c6bbcb7fe948583c8e1a1c4f5713ee
KChampagne <champagnekaleb@gmail.com>
2020-04-18 15:39:19 -0500

    Updates BackendResources, changed mapping to '/misc'

--- src/main/java/api/BackendResources.java
+++ src/main/java/api/BackendResources.java
@@ -1,12 +1,14 @@
 package api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping(value="/misc")
 public class BackendResources {
 
-    @GetMapping
+    @GetMapping("")
     public String helloGradle() {
         return "Hello Gradle!";
     }
