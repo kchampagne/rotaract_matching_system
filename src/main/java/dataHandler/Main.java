c92cec644fed797891344b8023ec98c313d8b048
KChampagne <champagnekaleb@gmail.com>
2020-04-26 12:05:00 -0500

    Updates Main, runs an init for Rotaractors in the database now. Adds resources for Rotaractors questions and responses

--- src/main/java/dataHandler/Main.java
+++ src/main/java/dataHandler/Main.java
@@ -8,18 +8,29 @@ import dataHandler.utils.SurveyRegistry;
 import java.util.List;
 
 public class Main {
-    public static String json = Const.projectDir + "\\src\\main\\resources\\rotarian_survey1-19-2020.json";
-    public static String csv = Const.projectDir + "\\src\\main\\resources\\rotarian_survey1-19-2020.csv";
+    public static String rotarian_json = Const.projectDir + "\\src\\main\\resources\\rotarian_survey1-19-2020.json";
+    public static String rotarian_csv = Const.projectDir + "\\src\\main\\resources\\rotarian_survey1-19-2020.csv";
+    public static String rotaract_json = Const.projectDir + "\\src\\main\\resources\\rotaract_survey4-19-2020.json";
+    public static String rotaract_csv = Const.projectDir + "\\src\\main\\resources\\rotaract_survey4-19-2020_edited.csv";
 
     public static void main(String[] args) {
-        initRotarians(json, csv);
+        initRotarians(rotarian_json, rotarian_csv);
+        initRotaractors(rotaract_json, rotaract_csv);
     }
 
-    public static void initRotarians(String json, String csv) {
+    public static void initRotarians(final String json, final String csv) {
         DbFunctions db = new DbFunctions();
 
-        SurveyRegistry.getInstance().registerFromJson(json);
+        SurveyRegistry.getRotaryInstance().registerFromJson(json);
         List<User> users = DataParser.parse(csv, User.UserType.Rotarian);
         db.createRotarians(users);
+    }
+
+    public static void initRotaractors(final String json, final String csv) {
+        DbFunctions db = new DbFunctions();
+
+        SurveyRegistry.getRotaractInstance().registerFromJson(json);
+        List<User> users = DataParser.parse(csv, User.UserType.Rotaractor);
+        db.createRotaractors(users);
     }
 }
